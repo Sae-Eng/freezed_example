2243604ca72f820d22f81ff2f5a30bcbf4637588
Sae-Eng <konata70@icloud.com>
2022-11-16 00:54:23 +0900

    feat: Create classes for API communication

---
+++ lib/api/user_request.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/user.dart';
+
+class UserRequest {
+  Uri url = Uri.parse('https://jsonplaceholder.typicode.com/users');
+
+  Future<List<User>> getAllUser() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode != 200) {
+      return <User>[];
+    }
+
+    List userMap = jsonDecode(response.body);
+
+    List<User> users = [];
+    userMap.forEach((user) {
+      users.add(User.fromJson(user));
+    });
+
+    return users;
+  }
+}
